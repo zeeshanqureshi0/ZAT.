242cc5354d793e54a7032d616d6af6c12461c74b
ZEESHANALTAF <115684396+zeeshanqureshi0@users.noreply.github.com>
2022-10-26 16:04:00 +0500

    Update ZAT.py

--- ZAT.py
+++ ZAT.py
@@ -132,7 +132,7 @@ def slowprint(s):
         sys.stdout.flush()
         time.sleep(8. / 100)
 print("\033[1;37m+-------------------------------------------------+")
-slowprint('''\033[1;37m|  Welcome To Zeeshan Altaf tricks           |
+slowprint('''\033[1;33m|  Welcome To Zeeshan Altaf tricks           |
 |           Subscribe Our YouTube Channel         |
 | Watch Ours Tutorials For Learn Zeeshan Altaf tricks  |''')
 print("+-------------------------------------------------+")
